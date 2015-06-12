469d3c59fac3fdcfadc4b0e325486fc6173c6ce7
Yuji Mise <miseyu2000@gmail.com>
2015-06-12 10:56:21 +0900

    [modify] rails env

--- lib/active_asari/common.rb
+++ lib/active_asari/common.rb
@@ -1,8 +1,8 @@
 module ActiveAsari
 
   def self.amazon_safe_domain_name(domain)
-    environment = ENV['RAILS_ENV'] ? ENV['RAILS_ENV'] : ENV['RACK_ENV'] 
-    "#{ACTIVE_ASARI_ENV[environment]['domain_prefix']}-#{domain.underscore.sub(/_/, '-')}" 
+    environment = Rails.env ? Rails.env : ENV['RACK_ENV'] 
+    "#{ACTIVE_ASARI_ENV[environment]['domain_prefix']}-#{domain.underscore.sub(/_/, '-')}"
   end
 
   def self.aws_client
@@ -13,11 +13,11 @@ module ActiveAsari
     $domains = {} if !$domains
     amazon_domain = amazon_safe_domain_name domain
     if !$domains[amazon_domain]
-      domain_data = aws_client.describe_domains[:domain_status_list].select { |domain_data|  
+      domain_data = aws_client.describe_domains[:domain_status_list].select { |domain_data|
         domain_data[:domain_name] == amazon_domain}
       $domains[amazon_domain] = domain_data.first[:search_service][:endpoint].split('.').first[7..-1]
     end
-    $domains[amazon_domain] 
+    $domains[amazon_domain]
   end
 
   def self.active_asari_raw_search(domain, query, search_options = {})
@@ -44,7 +44,7 @@ module ActiveAsari
 
   def self.configure(yaml_file_dir)
     active_asari_config = YAML.load_file(File.expand_path(yaml_file_dir) + '/active_asari_config.yml')
-    active_asari_env = YAML.load_file(File.expand_path(yaml_file_dir) + '/active_asari_env.yml') 
+    active_asari_env = YAML.load_file(File.expand_path(yaml_file_dir) + '/active_asari_env.yml')
     return active_asari_config, active_asari_env
   end
-end 
+end
